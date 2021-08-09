682bfa05d9405313db0f75873f45c270ec597f0e
joffy <joffy.jose.jfy@gmail.com>
2021-08-09 14:42:26 +0530

    initial commit

---
+++ src/main/java/com/joffy/jospalace/JospalaceApplication.java
@@ -0,0 +1,13 @@
+package com.joffy.jospalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JospalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JospalaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joffy/jospalace/JospalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joffy.jospalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JospalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
